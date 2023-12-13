8614dc28471f89df21ccd000c84ba9161e2f99e6
Leo Picado <leonardo.picado@zenjob.com>
2023-12-13 17:17:05 +0100

    Replaced textview with imageview

--- app/src/main/java/com/leopicado/diceroller/MainActivity.kt
+++ app/src/main/java/com/leopicado/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.leopicado.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 import com.leopicado.diceroller.databinding.ActivityMainBinding
@@ -21,8 +22,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.buttonRoll.text = "Let's Roll"
         binding.buttonRoll.setOnClickListener {
-            Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+            rollDice(binding)
         }
         setContentView(binding.root)
+    }
+
+    private fun rollDice(binding: ActivityMainBinding) {
+        val number = (1..6).random().toString()
+        binding.imageviewDice.setImageResource(
+            when (number) {
+                "1" -> R.drawable.dice_1
+                "2" -> R.drawable.dice_2
+                "3" -> R.drawable.dice_3
+                "4" -> R.drawable.dice_4
+                "5" -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        )
     }
 }
